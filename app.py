593b46536d65927ae5b53e6aa8ff06d233317f36
esther0717git <liangwy@sea.com>
2026-02-10 23:23:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 
 st.set_page_config(page_title="ClearID Check", layout="wide")
 st.title("🪪 ClearID Check")
-st.caption("Quickly identify new and removed personnel by comparing vendor rosters")
+st.caption("Quickly identify new and removed personnel(s) by comparing vendor rosters")
 
 NAME_COL = "Full Name As Per NRIC"
 SERIAL_COL = "S/N"
